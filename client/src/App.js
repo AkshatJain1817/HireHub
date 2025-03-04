eb34e92617ba2b10b5c695b242c6fe0ea05e369f
Akshat Jain <aj15018109@gmail.com>
2025-03-04 10:29:58 +0530

    Set up HireHub frontend with React

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to HireHub</h1>
+        <p>Your premier job board for employers and job seekers!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App
